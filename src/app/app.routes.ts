add018d98e8a96c2f878a5667f42cf24eb3d8342
samitha senewirathna <samithani17@gmail.com>
2025-04-29 14:10:56 +0530

    implement Ui layers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ManageEmployeeComponent } from './pages/manage-employee/manage-employee.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"manage-Enployee",
+        component:ManageEmployeeComponent
+    },
+    {
+        path:"logIn",
+        component:LoginComponent
+    },
+    {
+        path:"",
+        component:LoginComponent
+    }
+
+];
